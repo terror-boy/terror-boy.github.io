100a91cb86673368e12f8a0082cf7c2ea916b660
KTB <85664936+terror-boy@users.noreply.github.com>
2022-08-08 07:30:50 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'ktb/musics/Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'ktb/images/cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'ktb/musics/Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'ktb/images/cover 2.png'
+    },
+    // +6 more
+]
