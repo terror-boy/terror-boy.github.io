badf0656456cdd26a5f8526a6e1e999ac25bb13e
KTB <85664936+terror-boy@users.noreply.github.com>
2022-08-08 07:39:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,3 +19,50 @@ const changeCarousel = () => {
 setInterval(() => {
     changeCarousel();
 }, 3000);
+/////////////////////navigations////////////
+
+////////////toggling music player
+
+const musicPlayerSection = document.querySelector('.music-player-section');
+
+let clickCount = 1;
+
+musicPlayerSection.addEventListener('click', () => {
+    // checking for double click manually idk why default dbclick event was not working with this project If you know what could the problem Kindly tell me in the discussion below
+    if(clickCount >= 2){
+        musicPlayerSection.classList.add('active');
+        clickCount = 1;
+        return;
+    }
+    clickCount++;
+    setTimeout(() => {
+        clickCount = 1;
+    }, 250);
+})
+
+/////// back from music player
+
+const backToHomeBtn = document.querySelector('.music-player-section .back-btn');
+
+backToHomeBtn.addEventListener('click', () => {
+    musicPlayerSection.classList.remove('active');
+})
+
+//////// access playlist
+
+const playlistSection = document.querySelector('.playlist');
+const navBtn = document.querySelector('.music-player-section .nav-btn');
+
+navBtn.addEventListener('click', () => {
+    playlistSection.classList.add('active');
+})
+
+////////// back from playlist to music player
+
+const backToMusicPlayer = document.querySelector('.playlist .back-btn');
+
+backToMusicPlayer.addEventListener('click', () => {
+    playlistSection.classList.remove('active');
+})
+
+//////navigation done ////////////////
