05aacd6fe18d4d4b92fa0f640ea59cb21357a5e4
KTB <85664936+terror-boy@users.noreply.github.com>
2022-08-08 07:57:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,210 +1,147 @@
-///// carousels/////////////////
+let now_playing = document.querySelector(".now-playing");
+let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
 
-const carousel = [...document.querySelectorAll('.carousel img')];
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
 
-let carouselImageIndex = 0;
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
 
-const changeCarousel = () => {
-    carousel[carouselImageIndex].classList.toggle('active');
+let track_index = 0;
+let isPlaying = false;
+let updateTimer;
 
-    if(carouselImageIndex >= carousel.length - 1){
-        carouselImageIndex = 0;
-    } else{
-        carouselImageIndex++;
-    }
+// Create new audio element
+let curr_track = document.createElement('audio');
 
-    carousel[carouselImageIndex].classList.toggle('active');
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "Night Owl",
+    artist: "Broke For Free",
+    image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3"
+  },
+  {
+    name: "Enthusiast",
+    artist: "Tours",
+    image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3"
+  },
+  {
+    name: "Shipping Lanes",
+    artist: "Chad Crouch",
+    image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3",
+  },
+];
+
+function random_bg_color() {
+
+  // Get a number between 64 to 256 (for getting lighter colors)
+  let red = Math.floor(Math.random() * 256) + 64;
+  let green = Math.floor(Math.random() * 256) + 64;
+  let blue = Math.floor(Math.random() * 256) + 64;
+
+  // Construct a color withe the given values
+  let bgColor = "rgb(" + red + "," + green + "," + blue + ")";
+
+  // Set the background to that color
+  document.body.style.background = bgColor;
 }
 
-setInterval(() => {
-    changeCarousel();
-}, 3000);
-/////////////////////navigations////////////
+function loadTrack(track_index) {
+  clearInterval(updateTimer);
+  resetValues();
+  curr_track.src = track_list[track_index].path;
+  curr_track.load();
 
-////////////toggling music player
+  track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
+  track_name.textContent = track_list[track_index].name;
+  track_artist.textContent = track_list[track_index].artist;
+  now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
 
-const musicPlayerSection = document.querySelector('.music-player-section');
-
-let clickCount = 1;
-
-musicPlayerSection.addEventListener('click', () => {
-    // checking for double click manually idk why default dbclick event was not working with this project If you know what could the problem Kindly tell me in the discussion below
-    if(clickCount >= 2){
-        musicPlayerSection.classList.add('active');
-        clickCount = 1;
-        return;
-    }
-    clickCount++;
-    setTimeout(() => {
-        clickCount = 1;
-    }, 250);
-})
-
-/////// back from music player
-
-const backToHomeBtn = document.querySelector('.music-player-section .back-btn');
-
-backToHomeBtn.addEventListener('click', () => {
-    musicPlayerSection.classList.remove('active');
-})
-
-//////// access playlist
-
-const playlistSection = document.querySelector('.playlist');
-const navBtn = document.querySelector('.music-player-section .nav-btn');
-
-navBtn.addEventListener('click', () => {
-    playlistSection.classList.add('active');
-})
-
-////////// back from playlist to music player
-
-const backToMusicPlayer = document.querySelector('.playlist .back-btn');
-
-backToMusicPlayer.addEventListener('click', () => {
-    playlistSection.classList.remove('active');
-})
-
-//////navigation done ////////////////
-/////// music
-
-let currentMusic = 0;
-
-const music = document.querySelector('#audio-source');
-
-const seekBar = document.querySelector('.music-seek-bar');
-const songName = document.querySelector('.current-song-name');
-const artistName = document.querySelector('.artist-name');
-const coverImage = document.querySelector('.cover');
-const currentMusicTime = document.querySelector('.current-time');
-const musicDuration = document.querySelector('.duration');
-
-const queue = [...document.querySelectorAll('.queue')];
-
-// select all buttons here
-
-const forwardBtn = document.querySelector('i.fa-forward');
-const backwardBtn = document.querySelector('i.fa-backward');
-const playBtn = document.querySelector('i.fa-play');
-const pauseBtn = document.querySelector('i.fa-pause');
-const repeatBtn = document.querySelector('span.fa-redo');
-const volumeBtn = document.querySelector('span.fa-volume-up');
-const volumeSlider = document.querySelector('.volume-slider');
-That's a lot of selection, isn't it. Now setup music source.
-// funtion for setting up music
-
-const setMusic = (i) => {
-    seekBar.value = 0;
-    let song = songs[i];
-    currentMusic = i;
-
-    music.src = song.path;
-
-    songName.innerHTML = song.name;
-    artistName.innerHTML = song.artist;
-    coverImage.src = song.cover;
-
-    setTimeout(() => {
-        seekBar.max = music.duration;
-        musicDuration.innerHTML = formatTime(music.duration);
-    }, 300);
-    currentMusicTime.innerHTML = '00 : 00';
-    queue.forEach(item => item.classList.remove('active'));
-    queue[currentMusic].classList.add('active');
+  updateTimer = setInterval(seekUpdate, 1000);
+  curr_track.addEventListener("ended", nextTrack);
+  random_bg_color();
 }
 
-setMusic(0);
-// format duration in 00 : 00 format
-
-const formatTime = (time) => {
-    let min = Math.floor(time / 60);
-    if(min < 10){
-        min = `0` + min;
-    }
-
-    let sec = Math.floor(time % 60);
-    if(sec < 10){
-        sec = `0` + sec;
-    }
-
-    return `${min} : ${sec}`;
+function resetValues() {
+  curr_time.textContent = "00:00";
+  total_duration.textContent = "00:00";
+  seek_slider.value = 0;
 }
-// playBtn click event
 
-playBtn.addEventListener('click', () => {
-    music.play();
-    playBtn.classList.remove('active');
-    pauseBtn.classList.add('active');
-})
+// Load the first track in the tracklist
+loadTrack(track_index);
 
+function playpauseTrack() {
+  if (!isPlaying) playTrack();
+  else pauseTrack();
+}
 
-// pauseBtn click event
+function playTrack() {
+  curr_track.play();
+  isPlaying = true;
+  playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
 
-pauseBtn.addEventListener('click', () => {
-    music.pause();
-    pauseBtn.classList.remove('active');
-    playBtn.classList.add('active');
-})
-//  forward btn
+function pauseTrack() {
+  curr_track.pause();
+  isPlaying = false;
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
+}
 
-forwardBtn.addEventListener('click', () => {
-    if(currentMusic >= songs.length - 1){
-        currentMusic = 0;
-    } else{
-        currentMusic++;
-    }
-    setMusic(currentMusic);
-    playBtn.click();
-})
+function nextTrack() {
+  if (track_index < track_list.length - 1)
+    track_index += 1;
+  else track_index = 0;
+  loadTrack(track_index);
+  playTrack();
+}
 
-// backward btn
+function prevTrack() {
+  if (track_index > 0)
+    track_index -= 1;
+  else track_index = track_list.length;
+  loadTrack(track_index);
+  playTrack();
+}
 
-backwardBtn.addEventListener('click', () => {
-    if(currentMusic <= 0){
-        currentMusic = songs.length - 1;
-    } else{
-        currentMusic--;
-    }
-    setMusic(currentMusic);
-    playBtn.click();
-})
-                         // seekbar events
+function seekTo() {
+  let seekto = curr_track.duration * (seek_slider.value / 100);
+  curr_track.currentTime = seekto;
+}
 
-setInterval(() => {
-    seekBar.value = music.currentTime;
-    currentMusicTime.innerHTML = formatTime(music.currentTime);
-    if(Math.floor(music.currentTime) == Math.floor(seekBar.max)){
-        if(repeatBtn.className.includes('active')){
-            setMusic(currentMusic);
-            playBtn.click();
-        } else{
-            forwardBtn.click();
-        }
-    }
-}, 500)
+function setVolume() {
+  curr_track.volume = volume_slider.value / 100;
+}
 
-seekBar.addEventListener('change', () => {
-    music.currentTime = seekBar.value;
-})
-      // repeat button
+function seekUpdate() {
+  let seekPosition = 0;
 
-repeatBtn.addEventListener('click', () => {
-    repeatBtn.classList.toggle('active');
-})
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
 
-// volume section
+    seek_slider.value = seekPosition;
 
-volumeBtn.addEventListener('click', () => {
-    volumeBtn.classList.toggle('active');
-    volumeSlider.classList.toggle('active');
-})
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
 
-volumeSlider.addEventListener('input', () => {
-    music.volume = volumeSlider.value;
-})
-      queue.forEach((item, i) => {
-    item.addEventListener('click', () => {
-        setMusic(i);
-        playBtn.click();
-    })
-})
+    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
+
