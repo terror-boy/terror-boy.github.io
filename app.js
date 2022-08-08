70d62cc2c0217f6bc8c85936bdd3b842a4547c03
KTB <85664936+terror-boy@users.noreply.github.com>
2022-08-08 10:42:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,12 +21,7 @@ let curr_track = document.createElement('audio');
 
 // Define the tracks that have to be played
 let track_list = [
-  {
-    name: "Night Owl",
-    artist: "Broke For Free",
-    image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
-    path: "https://open.spotify.com/playlist/3NcxM1LJJdua8AcRxtijNY"
-  },
+  
   {
     name: "Night Owl",
     artist: "Broke For Free",
